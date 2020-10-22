907d07b976524cd74cd2780b3a34d8a6f347c3c9
Vladislav Babkin <vladiksonic@gmail.com>
2020-10-22 06:13:12 +0300

    Update photo-swipe.js

--- addon/components/photo-swipe.js
+++ addon/components/photo-swipe.js
@@ -174,7 +174,7 @@ export default Component.extend({
 
     assignedOptions = assign({}, options, actionOptions);
     pswp = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, assignedOptions);
-    assignedOptions.pswp = pwsp;
+    assignedOptions.pswp = pswp;
     pswp.listen('gettingData', function(index, item) {
       if (item.w < 1 || item.h < 1) { // unknown size
         var img = new Image();
